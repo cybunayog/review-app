994dabaa46f4021ddbf3e38f3ac99a54f29e304e
Cy Bunayog <cybunayog@gmail.com>
2020-10-07 19:43:29 -0700

    Protecting the API Endpoints

---
+++ server/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+module.exports = function (req, res, next) {
+  if (req.headers && req.headers.authorization) {
+    try {
+      req.user = jwt.verify(req.headers.authorization.split(" ")[1], config.JWT_SECRET);
+    } catch (e) {
+      return res.status(401).json({
+        error: {
+          msg: 'Failed to authenticate token!'
+        }
+      })
+    }
+  } else {
+    return res.status(401).json({
+      error: {
+        msg: 'No token!'
+      }
+    })
+  }
+
+  return next();
+};
